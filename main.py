e0030e66e6356790794bf673c2d5c10acfaf07ac
Vikash Oraon <72189417+VikashOraon@users.noreply.github.com>
2023-08-16 12:29:48 +0530

    maintain aspect ratio
    images will now maintain the original aspect ration

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from PIL import Image
 import os
 
-def convert_jpeg_to_webp(input_path, output_path, new_width, new_height, quality=80):
+
+
+def resize_to_fit(img, max_width, max_height):
+    width, height = img.size
+    aspect_ratio = width / height
+
+    if width > max_width or height > max_height:
+        if aspect_ratio > 1:
+            new_width = max_width
+            new_height = int(max_width / aspect_ratio)
+        else:
+            new_height = max_height
+            new_width = int(max_height * aspect_ratio)
+
+        return img.resize((new_width, new_height), Image.LANCZOS)
+    return img
+
+
+def convert_jpeg_to_webp(input_path, output_path, max_width, max_height, quality=80):
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
@@ -11,22 +29,22 @@ def convert_jpeg_to_webp(input_path, output_path, new_width, new_height, quality
             output_file = os.path.join(output_path, os.path.splitext(filename)[0] + '.webp')
 
             img = Image.open(input_file)
-            resized_img = img.resize((new_width, new_height), Image.LANCZOS)
+            resized_img = resize_to_fit(img, max_width, max_height)
             resized_img.save(output_file, 'webp', quality=quality)
             print(f"Converted and resized {input_file} to {output_file}")
 
 if __name__ == "__main__":
-    # Replace with the path to your "Album" directory
-    album_directory = "images"
-    # New width for resized images
-    new_width = 1920
-    # New height for resized images
-    new_height = 1080
+    # Replace with the path to your "images" directory
+    image_directory = "images"
+    # max width for resized images
+    max_width = 1920
+    # max height for resized images
+    max_height = 1080
     # Adjust the quality as needed (0-100)
     quality = 80
     # images/[...] --> output/[...]
-    for subfolder_name in os.listdir(album_directory):
-        subfolder_path = os.path.join(album_directory, subfolder_name)
+    for subfolder_name in os.listdir(image_directory):
+        subfolder_path = os.path.join(image_directory, subfolder_name)
         if os.path.isdir(subfolder_path):
             output_directory = os.path.join("output", subfolder_name)
-            convert_jpeg_to_webp(subfolder_path, output_directory, new_width, new_height, quality)
+            convert_jpeg_to_webp(subfolder_path, output_directory, max_width, max_height, quality)
